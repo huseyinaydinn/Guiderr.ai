a45f0e6f2713deec590e0db1f1e5aeb82fa85456
Hüseyin Aydın <hsynaydino@gmail.com>
2025-04-04 00:20:11 +0300

    Footer Address Update

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -2,8 +2,12 @@ import Image from "next/image"
 import Link from "next/link"
 import React from 'react'
 import Logo from '@/public/Guider_Logo.svg'
-import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaAddressCard, FaBuilding } from "react-icons/fa";
 import { AiFillTwitterCircle } from "react-icons/ai";
+import { FaMapLocationDot } from "react-icons/fa6";
+import { IoPersonCircle } from "react-icons/io5";
+
+
 
 
 const Footer = () => {
@@ -11,12 +15,57 @@ const Footer = () => {
         <section id="#faqs" className="relative overflow-hidden lg:overflow-visible mx-auto">
             {/*Footer Links */}
 
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8 xl:px-12 2xl:px-16 py-8 flex flex-col lg:flex-row justify-between items-center text-center gap-8">
+            <div className="w-full container mx-auto px-4 sm:px-6 lg:px-8 xl:px-12 2xl:px-16 py-8 flex flex-col lg:flex-row justify-between items-center text-center gap-8">
                 {/* icon area */}
-                <div className="max-w-[50%] flex flex-col items-center text-center lg:items-start lg:text-start gap-4">
-                    <Link href="#home" >
+                <div className="flex flex-col items-start text-center lg:items-start lg:text-start gap-4 lg:max-w-[50%]">
+                    <Link href="#home" className="mb-10" >
                         <Image src={Logo} className="w-16 md:w-20 lg:w-24" alt="Logo" />
                     </Link>
+
+                    <div className="flex flex-row items-center gap-2 text-start">
+                        <div className="flex flex-row items-center justify-between w-[120px] gap-2 ">
+                            <div className="font-bold flex flex-row items-center gap-2">
+                                <FaMapLocationDot className="w-6 h-6" />
+                                <h5 className="text-sm">Address</h5>
+                            </div>
+                            <p className="font-bold text-lg">:</p>
+                        </div>
+                        <p className="text-sm">PLOT NO 134, 2ND FLOOR NESPAK C-1, COLLEGE ROAD GHAZI CHOWK, Punjab, Lahore, 54000, Pakistan</p>
+                    </div>
+
+                    <div className="flex flex-row items-center gap-2">
+                        <div className="flex flex-row items-center justify-between w-[120px]">
+                            <div className="font-bold flex flex-row items-center gap-2">
+                                <FaAddressCard className="w-6 h-6" />
+                                <h5 className="text-sm">Phone number</h5>
+                            </div>
+                            <p className="font-bold text-lg">:</p>
+                        </div>
+                        <p className="text-sm">+92 3317380356</p>
+                    </div>
+
+                    <div className="flex flex-row items-center gap-2">
+                        <div className="flex flex-row items-center justify-between w-[120px] ">
+                            <div className="font-bold flex flex-row items-center gap-2">
+                                <IoPersonCircle className="w-6 h-6" />
+                                <h5 className="text-sm">Managing Director</h5>
+                            </div>
+                            <p className="font-bold text-lg">:</p>
+                        </div>
+                        <p className="text-sm">Muhammad Hassan Farooq</p>
+                    </div>
+
+                    <div className="flex flex-row items-center gap-2">
+                        <div className="flex flex-row items-center justify-between w-[120px] ">
+                            <div className="font-bold flex flex-row items-center gap-2">
+                                <FaBuilding className="w-6 h-6" />
+                                <h5 className="text-sm">Product of</h5>
+                            </div>
+                            <p className="font-bold text-lg">:</p>
+                        </div>
+                        <p className="text-sm">Zirvesol</p>
+                    </div>
+
                     <p>Discover destinations, get AI-powered recommendations, and plan your perfect trip effortlessly.</p>
 
                 </div>
